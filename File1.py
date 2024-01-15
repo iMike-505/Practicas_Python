6b691bd31c07346d43914657641499e55ee5339b
Miguel Contreras <imike505dm@gmail.com>
2024-01-14 23:41:25 -0600

    ADD NEW CODE

--- File1.py
+++ File1.py
@@ -6,3 +6,14 @@ print(len(Lista_1))
 Lista_1 = [12, 22, 8, 78, 66, 30, 75]
 
 print(Lista_1)
+print(len(Lista_1))
+
+Lista_2 = [94, 21, 40, 85, 30, "Apolo", "Artemis"]
+
+# En una lista podemos acceder a los elementos por separado
+print(Lista_2[5])
+
+print(Lista_2[0])
+print(Lista_2[6])
+print(Lista_2[-1])
+print(Lista_2[-7])
